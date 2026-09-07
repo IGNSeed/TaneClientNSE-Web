6b2e259b8544868ecb65ecf974fed5bc9a4c3dca
IGNSeed <n.yuuga0803@gmail.com>
2026-09-08 02:45:01 +0900

    release: TaneClient NSE v1.0.8を公開準備
    Update the stable manifest and website download metadata for the SaltyNX/FPSLocker compatibility release.

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,8 +1,8 @@
 window.TaneClientRelease = Object.freeze({
-  version: "1.0.7",
-  tag: "v1.0.7",
+  version: "1.0.8",
+  tag: "v1.0.8",
   repositoryUrl: "https://github.com/IGNSeed/TaneClientNSE-Web",
-  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.0.7",
-  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.0.7/TaneClientNSE-v1.0.7.zip",
-  filename: "TaneClientNSE-v1.0.7.zip"
+  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.0.8",
+  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.0.8/TaneClientNSE-v1.0.8.zip",
+  filename: "TaneClientNSE-v1.0.8.zip"
 });
