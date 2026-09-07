459bdd5ed9a711575127b0ed7fc5317ebf246883
IGNSeed <n.yuuga0803@gmail.com>
2026-09-07 19:56:18 +0900

    release: v1.0.4配布情報を準備 / prepare v1.0.4 release metadata

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,8 +1,8 @@
 window.TaneClientRelease = Object.freeze({
-  version: "1.0.3",
-  tag: "v1.0.3",
+  version: "1.0.4",
+  tag: "v1.0.4",
   repositoryUrl: "https://github.com/IGNSeed/TaneClientNSE-Web",
-  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.0.3",
-  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.0.3/TaneClientNSE-v1.0.3.zip",
-  filename: "TaneClientNSE-v1.0.3.zip"
+  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.0.4",
+  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.0.4/TaneClientNSE-v1.0.4.zip",
+  filename: "TaneClientNSE-v1.0.4.zip"
 });
