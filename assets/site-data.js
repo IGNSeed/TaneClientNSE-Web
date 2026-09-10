c9c60caa35c02e913babf78577d84210f635e16f
IGNSeed <n.yuuga0803@gmail.com>
2026-09-11 01:48:19 +0900

    release: TaneClient NSE v1.1.3を公開準備 / Prepare TaneClient NSE v1.1.3 release

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,8 +1,8 @@
 window.TaneClientRelease = Object.freeze({
-  version: "1.1.2",
-  tag: "v1.1.2",
+  version: "1.1.3",
+  tag: "v1.1.3",
   repositoryUrl: "https://github.com/IGNSeed/TaneClientNSE-Web",
-  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.1.2",
-  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.1.2/TaneClientNSE-v1.1.2.zip",
-  filename: "TaneClientNSE-v1.1.2.zip"
+  releaseUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/tag/v1.1.3",
+  downloadUrl: "https://github.com/IGNSeed/TaneClientNSE-Web/releases/download/v1.1.3/TaneClientNSE-v1.1.3.zip",
+  filename: "TaneClientNSE-v1.1.3.zip"
 });
